f6856122090c54359ffc33c0c949a2439d777780
Rene Montoya <rmontoya.we@gmail.com>
2020-11-12 22:57:55 -0800

    finishing most of dojo survey with bonuses

--- dojo_survey/views.py
+++ dojo_survey/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    context = {
+        "message": "This the message"  #kore wa nani wo shimasuka
+    }
+    return render(request, "index.html", context)
+
+def result(request):
+    if request.method == "GET":
+        return redirect('/')
+    if request.method == "POST":
+        # request.session['']
+        context = {
+            'name': request.POST['name'],
+            'location': request.POST['location'],
+            'proglanguage': request.POST['proglanguage'],
+            'laptop' : request.POST['laptop'],
+            'comment': request.POST['comment'],
+            'math' : request.POST['math'],
+        }
+        return render(request, "results.html", context)
+    return render(request, "result.html")
