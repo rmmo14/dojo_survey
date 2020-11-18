97f268df35884fb367630bf9be982e10e85a1af6
Rene Montoya <rmontoya.we@gmail.com>
2020-11-17 16:59:31 -0800

    dojo survey revisited and redirected

--- dojo_survey/views.py
+++ dojo_survey/views.py
@@ -2,23 +2,26 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    context = {
-        "message": "This the message"  #kore wa nani wo shimasuka
-    }
-    return render(request, "index.html", context)
+    # context = {
+    #     "message": "This the message"  #kore wa nani wo shimasuka
+    # }
+    # return render(request, "index.html", context)
+    print('got here from redirect')
+    return render(request, 'index.html')
 
 def result(request):
-    if request.method == "GET":
-        return redirect('/')
-    if request.method == "POST":
+    # if request.method == "GET":
+    #     return redirect('/')
+    # if request.method == "POST":
         # request.session['']
-        context = {
-            'name': request.POST['name'],
-            'location': request.POST['location'],
-            'proglanguage': request.POST['proglanguage'],
-            'laptop' : request.POST['laptop'],
-            'comment': request.POST['comment'],
-            'math' : request.POST['math'],
-        }
-        return render(request, "results.html", context)
-    return render(request, "result.html")
+        
+    request.session['name']= request.POST['name']
+    request.session['location']= request.POST['location']
+    request.session['proglanguage']= request.POST['proglanguage']
+    request.session['laptop'] = request.POST['laptop']
+    request.session['comment']= request.POST['comment']
+    request.session['math'] = request.POST['math']
+        
+    #     return render(request, "results.html")
+    # return render(request, "results.html")
+    return redirect('/')
